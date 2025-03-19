87b73460b56d9d71bae1c6c92abd5a44c505660f
luomo <luomo@fake.com>
2025-03-19 09:23:50 +0800

    Fix alpine entrypoint.sh error https://github.com/V-Shadbolt/docker-strapi/issues/5

--- images/strapi-alpine/docker-entrypoint.sh
+++ images/strapi-alpine/docker-entrypoint.sh
@@ -114,7 +114,7 @@ module.exports = ({env}) => ([
 ]);
 EOT
 
-  elif [ ! -d "node_modules" ] || [ ! "$(ls -qAL node_modules 2>/dev/null)" ]; then
+  elif [ ! -d "node_modules" ] || [ ! "$(ls -AL node_modules 2>/dev/null)" ]; then
     echo "Node modules not installed. Installing ..."
     if [ -f "yarn.lock" ]; then
       yarn install --prod
